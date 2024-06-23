aecd9c841e2c7c6f28fb4305515027fdb5ab9619
MikaTsur <mikatsur@gmail.com>
2024-06-23 09:16:25 +0300

    working buttons

---
+++ client/src/components/HeaderButtons.jsx
@@ -0,0 +1,29 @@
+// ex\client\src\components\HeaderButtons.jsx
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const HeaderButtons = () => {
+  const navigate = useNavigate();
+
+  const navigateToMovies = () => {
+    navigate("/movies");
+  };
+
+  const navigateToAddMovie = () => {
+    navigate("/add-movie");
+  };
+
+  return (
+    <div style={{ marginBottom: "10px" }}>
+      <button style={{ marginRight: "10px" }} onClick={navigateToMovies}>
+        All Movies
+      </button>
+      <button style={{ marginRight: "10px" }} onClick={navigateToAddMovie}>
+        Add Movie
+      </button>
+    </div>
+  );
+};
+
+export default HeaderButtons;
---
+++ client/src/pages/AddMovie.jsx
@@ -0,0 +1,16 @@
+// ex\client\src\pages\AddMovie.jsx
+
+import React from "react";
+import HeaderButtons from "../components/HeaderButtons";
+
+const AddMovie = () => {
+  return (
+    <>
+      <h3>Add Movie</h3>
+      <HeaderButtons />
+      {/* Add your form or input fields to add a new movie */}
+    </>
+  );
+};
+
+export default AddMovie;
