d4f6ab1be6a9eb24160844786552b269f7547ba5
MikaTsur <mikatsur@gmail.com>
2024-06-26 14:42:50 +0300

    UpperLevelButtons

---
+++ client/src/components/UpperLevelButtons.jsx
@@ -0,0 +1,45 @@
+//C:\Users\morellyo\react_project\ex\client\src\components\UpperLevelButtons.jsx
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const UpperLevelButtons = () => {
+  const navigate = useNavigate();
+
+  const navigateToMovies = () => {
+    navigate("/movies");
+  };
+
+  const navigateToSubscrptions = () => {
+    navigate("/Subscrptions");
+  };
+
+  const navigateToUserManagement = () => {
+    navigate("/UserManagement");
+  };
+
+  const navigateToLogOut = () => {
+    navigate("/LogOut");
+  };
+
+  return (
+    <div style={{ marginBottom: "10px" }}>
+      <button style={{ marginRight: "10px" }} onClick={navigateToMovies}>
+        All Movies
+      </button>
+      <button style={{ marginRight: "10px" }} onClick={navigateToSubscrptions}>
+        Subscrptions
+      </button>
+      <button
+        style={{ marginRight: "10px" }}
+        onClick={navigateToUserManagement}
+      >
+        User Management
+      </button>
+      <button style={{ marginRight: "10px" }} onClick={navigateToLogOut}>
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default UpperLevelButtons;
